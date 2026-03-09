4eced37eb21a2a31ec81f0fda93dbcb4d6c7df24
Acui6984 <edwin.torres64@upr.edu>
2026-03-09 14:12:07 -0400

    Reset() Paso 1. Borra todos los enemigos anteriores
    Paso 2. Reinicia el jugador
    
    Paso3. Reinicia variables del juego.
    
    Lo nuevo es:
    
    paso 4. Añade dos enemigos especiales
    
    Paso 5. Crea 30 enemigos normales
    
    Lo que hice fue simplemente copiar el mismo código que está en el constructor Program() y pegarlo dentro de Reset() después de Enemy::enemies.clear().

--- src/Program.cpp
+++ src/Program.cpp
@@ -191,4 +191,24 @@ void Program::Reset() {
     count = 0;
     delay = 0;
     lives = 3;
+
+    Enemy::enemies.push_back(std::pair<std::pair<float, float>, Enemy*> {
+        std::pair<float, float>{350, 150}, 
+        new SpEnemy(350, 150)
+    });
+
+    Enemy::enemies.push_back(std::pair<std::pair<float, float>, Enemy*> {
+        std::pair<float, float>{600, 150}, 
+        new SpEnemy(600, 150)
+    });
+
+    for (int i = 0; i < 30; i++) {
+        float x = 250 + 50 *(i%10);
+        float y = 200 + 50 *(i/10);
+
+        Enemy::enemies.push_back(std::pair<std::pair<float, float>, Enemy*> {
+            std::pair<float, float>{x, y}, 
+            new StdEnemy(x, y)
+        });
+    }
 }
